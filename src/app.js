0c5b5c6a7beeb8bb19cf2ce961d23df638f81e15
Devin Lewtan <devinlewtan@gmail.com>
2020-03-31 12:22:26 -0400

    part 3 completed: filtering

--- src/app.js
+++ src/app.js
@@ -12,8 +12,6 @@ app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: false }));
 
-//const test = new Review(courseName: "CSCI-UA.2020", "Intro To Devin", "Spring", 2020, "Ms. Lewtan", "the big quar, huh")
-
 const test = new Review ({
 	courseNumber: "CSCI-UA.2020",
   courseName: "Intro To Devin",
@@ -26,48 +24,50 @@ const test = new Review ({
 // app.get('/', (req, res) => {
 //   res.send(test.courseName)
 // });
-let newReviews = [];
-
-Review.find({}, (err, reviews) => {
-  newReviews = reviews;
-  //res.render('all', {reviews: newReviews});
-});
+// let newReviews = [];
+//
+// Review.find({}, (err, reviews) => {
+//   newReviews = reviews;
+//   //res.render('all', {reviews: newReviews});
+// });
 
 
 app.get('/', (req, res) => {
   // shows form
+  const { semester, year, professor } = req.query;
   // show reviews too
-  Review.find({}, (err, reviews) => {
-    res.render('all', {reviews: newReviews});
+	let query = {}
+	if (semester !== undefined && semester !== "" && semester !== "All") {
+		query.semester = semester
+	}
+	if (year !== undefined && year !== "") {
+		query.year = year
+	}
+	if (professor !== undefined && professor !== "") {
+		query.professor = professor
+	}
+	console.log(query)
+  Review.find(query, (err, reviews) =>
+    res.render('all', {reviews: reviews})
+  )
   });
-});
 
-app.get('/add', (req, res) => {
-  // shows form
-    res.render('review');
-});
 
-app.post('/add', (req, res) => {
-  Review.find({}, (err, reviews) => {
-    const { courseNumber, courseName, semester, year, professor, review } = req.body
-    newReviews = [new Review({ courseNumber, courseName, semester, year, professor, review }), ...reviews]
-    res.render('all', {reviews: newReviews});
-  });
-});
+// app.get('/add', (req, res) => {
+//   // shows form
+//     res.render('review');
+// });
+//
+// app.post('/add', (req, res) => {
+//   Review.find({}, (err, reviews) => {
+//     const { courseNumber, courseName, semester, year, professor, review } = req.body
+//     newReviews = [new Review({ courseNumber, courseName, semester, year, professor, review }), reviews]
+//     res.render('all', {reviews: newReviews});
+//   });
+// });
 
 
 //const dataPath = path.join(__dirname, 'data.json');
   const port = 3000;
   app.listen(port);
   console.log(`server started on port ${port}`);
-
-
-//   db.createUser(
-//   {
-//     user: "djl",
-//     pwd: "lewtan", // or cleartext password
-//     roles: [ { role: "userAdminAnyDatabase", db: "admin" }, "readWriteAnyDatabase" ]
-//   }
-// )
-
-// mongo --port 27017  --authenticationDatabase "admin" -u "djl" -p
